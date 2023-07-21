190bc5784f0f4c1acc44a67d22832facc2d6206f
Vik <seqaaa@gmail.com>
2023-07-21 12:46:59 +0300

    test

--- index.ts
+++ index.ts
@@ -19,22 +19,22 @@ app.use(
 app.get("/", (request, response) => {
   response.json("Hello world");
 });
-app.use("/api", router);
-app.use(errorMiddleware);
+// app.use("/api", router);
+// app.use(errorMiddleware);
 
-const port = process.env.PORT || "";
+const port = process.env.PORT || 5000;
 
-// respond with "hello world" when a GET request is made to the homepage
+// // respond with "hello world" when a GET request is made to the homepage
 
-const start = async () => {
-  try {
-    await mongoose.connect(process.env.DB_URL);
-  } catch (error) {
-    console.log("error:", error);
-  }
-};
+// const start = async () => {
+//   try {
+//     await mongoose.connect(process.env.DB_URL);
+//   } catch (error) {
+//     console.log("error:", error);
+//   }
+// };
 
-start();
+// start();
 
 app.listen(port, function () {
   console.log("Example app listening on port " + port + "!");
