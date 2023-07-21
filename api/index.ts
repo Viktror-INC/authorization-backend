a86c70bc2dfbbe6706eb567d203f2e8a9fc4853d
Vik <seqaaa@gmail.com>
2023-07-21 12:50:31 +0300

    test

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,43 @@
-const app = require('express')();
-const { v4 } = require('uuid');
+import "dotenv/config";
+// import router from "./src/router";
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+import * as express from "express";
+import * as cors from "cors";
+import * as mongoose from "mongoose";
+import * as cookieParser from "cookie-parser";
+// import errorMiddleware from "./src/middleware/error-middleware";
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
+app.get("/", (request, response) => {
+  response.json("Hello world");
+});
+// app.use("/api", router);
+// app.use(errorMiddleware);
+
+const port = process.env.PORT || 5000;
+
+// // respond with "hello world" when a GET request is made to the homepage
+
+// const start = async () => {
+//   try {
+//     await mongoose.connect(process.env.DB_URL);
+//   } catch (error) {
+//     console.log("error:", error);
+//   }
+// };
+
+// start();
+
+app.listen(port, function () {
+  console.log("Example app listening on port " + port + "!");
 });
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
-
-module.exports = app;
+export default app;
