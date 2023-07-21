b759b3c63283119eac351a0d90dc67b512466961
Vik <seqaaa@gmail.com>
2023-07-21 15:26:50 +0300

    add fix for vercel

--- index.ts
+++ index.ts
@@ -11,12 +11,6 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use(
   cors({
     credentials: true,
