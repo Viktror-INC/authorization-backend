6f643ac05a2d3a9a2ee08b36b7da8a4cf822e0c6
vik <seqaaa@gmail.com>
2023-07-22 18:35:48 +0300

    change domain

--- index.ts
+++ index.ts
@@ -6,10 +6,21 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const session = require('express-session');
+
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(session({
+  secret: 'yoursecret',
+  cookie: {
+      path: '/',
+      domain: 'localhost',
+      maxAge: 1000 * 60 * 24 // 24 hours
+  }
+}));
 
 app.use(
   cors({
@@ -17,11 +28,21 @@ app.use(
     origin: [process.env.CLIENT_URL, process.env.PROD_URL],
   })
 );
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
+
 app.get("/", (request, response) => {
   response.json("Hello world");
 });
 app.use("/api", router);
 app.use(errorMiddleware);
+
 
 const port = process.env.PORT || 5000;
 
